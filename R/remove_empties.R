615d84f655b47f55940e312007b1d8c492160385
Sam <samuel.firke@gmail.com>
2016-04-12 19:34:22 -0400

    adding functions to drop empty cols & rows

---
+++ R/remove_empties.R
@@ -0,0 +1,28 @@
+#' @title Removes empty rows from a data.frame.
+#'
+#' @description
+#' Removes all rows from a data.frame that are composed entirely of \code{NA} values.
+#'
+#' @param dat the input data.frame.
+#' @return Returns the data.frame with no empty rows.
+#' @examples
+#' remove_empty_rows(dat)
+
+remove_empty_rows <- function(dat){
+  dat[rowSums(is.na(dat)) != ncol(dat), ]
+}
+
+#' @title Removes empty columns from a data.frame.
+#'
+#' @description
+#' Removes all columns from a data.frame that are composed entirely of \code{NA} values.
+#'
+#' @param dat the input data.frame.
+#' @return Returns the data.frame with no empty columns.
+#' @examples
+#' remove_empty_cols(dat)
+#'
+
+remove_empty_cols <- function(dat){
+  dat[colSums(!is.na(dat)) > 0]
+}
