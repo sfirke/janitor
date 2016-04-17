7909e33437ed2c64485bb62abcfdb9ff0f3ba54e
Sam <samuel.firke@gmail.com>
2016-04-17 13:24:53 -0400

    make date_system argument consistent in excel_numeric_to_date

--- R/excel_dates.R
+++ R/excel_dates.R
@@ -7,7 +7,7 @@
 #' More on date encoding systems at http://support.office.com/en-us/article/Date-calculations-in-Excel-e7fe7167-48a9-4b96-bb53-5612a800b487.
 #'
 #' @param date_num numeric vector of serial numbers to convert.
-#' @param modern the date system, either \code{"modern"} or \code{"mac pre-2011"}.
+#' @param date_system the date system, either \code{"modern"} or \code{"mac pre-2011"}.
 #' @return Returns a vector of class Date.
 #' @export
 #' @examples
@@ -15,12 +15,12 @@
 
 # Converts a numeric value like 42414 into a date "2016-02-14"
 
-excel_numeric_to_date <- function(date_num, created = "modern") {
+excel_numeric_to_date <- function(date_num, date_system = "modern") {
   if(!is.numeric(date_num)){ stop("argument `date_num` must be of class numeric") }
 
-  if(created == "mac pre-2011"){
+  if(date_system == "mac pre-2011"){
     as.Date(date_num,  origin = "1904-01-01")
-  } else if(created == "modern"){
+  } else if(date_system == "modern"){
     as.Date(date_num, origin = "1899-12-30")
   } else {
     stop("argument 'created' must be one of 'mac pre-2011' or 'modern'")
