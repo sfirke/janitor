fd2072de146e1a1ea4f0eb66fc697121ca793115
Sam <samuel.firke@gmail.com>
2016-05-03 06:27:07 -0400

    add tests for excel date conversion

---
+++ tests/testthat/test-date-conversion.R
@@ -0,0 +1,14 @@
+# Tests for Excel date cleaning function
+
+library(janitor)
+context("Excel date cleaning")
+
+test_that("Serial number dates convert correctly", {
+  expect_equal(excel_numeric_to_date(42370), as.Date("2016-01-01"))
+  expect_equal(excel_numeric_to_date(42370, "modern"), as.Date("2016-01-01"))
+  expect_equal(excel_numeric_to_date(40908, "mac pre-2011"), as.Date("2016-01-01"))
+})
+
+test_that("Bad inputs handled appropriately", {
+  expect_error(excel_numeric_to_date("hello"), "argument `date_num` must be of class numeric")
+})
