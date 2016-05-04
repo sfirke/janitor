6f6d0b9ccbde5b3b01e5218ca2d4a7af71c48226
Sam <samuel.firke@gmail.com>
2016-05-04 16:51:09 -0400

    import everything() from dplyr
    trying to fix Travis failure

--- R/get_dupes.R
+++ R/get_dupes.R
@@ -1,5 +1,5 @@
 #' @importFrom magrittr "%>%"
-#'
+#' @importFrom dplyr everything
 #' @title Get rows of a \code{data.frame} with identical values for the specified variables.
 #'
 #' @description
@@ -21,7 +21,7 @@ get_dupes <- function(dat, ...) {
     dplyr::group_by(...) %>%
     dplyr::filter(n() > 1)  %>%
     dplyr::mutate(dupe_count = n()) %>%
-    dplyr::select(..., dupe_count, dplyr:::everything()) %>%
+    dplyr::select(..., dupe_count, everything()) %>%
     dplyr::ungroup() %>%
     dplyr::arrange(...)
 
