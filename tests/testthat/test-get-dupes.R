64ce90dd7f84e79c10874ab352c7cf0e34d32ab9
Sam Firke <samuel.firke@gmail.com>
2016-05-26 19:42:59 -0400

    add tests for get_dupes

--- tests/testthat/test-get-dupes.R
+++ tests/testthat/test-get-dupes.R
@@ -1,11 +1,23 @@
 # Tests for Excel date cleaning function
 
 library(janitor)
-context("Duplicate identification")
+context("duplicate identification")
 
-test_df <- data.frame(a = c(1,3,3,3,5), b = c("a", "c","c", "e", "c"))
+library(dplyr)
+test_df <- data.frame(a = c(1,3,3,3,5), b = c("a", "c","c", "e", "c"), stringsAsFactors = FALSE)
+
 test_that("Correct combinations of duplicates are found", {
-  expect_equal(excel_numeric_to_date(42370), as.Date("2016-01-01"))
-  expect_equal(excel_numeric_to_date(42370, "modern"), as.Date("2016-01-01"))
-  expect_equal(excel_numeric_to_date(40908, "mac pre-2011"), as.Date("2016-01-01"))
+  expect_equal(get_dupes(test_df, a), data_frame(a = test_df[[1]][2:4], dupe_count = rep(3L, 3), b = test_df[[2]][2:4]))
+  expect_equal(get_dupes(test_df, b), data_frame(b = test_df[[2]][c(2:3,5)], dupe_count = rep(3L, 3), a = test_df[[1]][c(2:3,5)]))
+  })
+
+test_that("calling with no specified variable names uses all variable names", {
+  # function does not yet do this
 })
+
+no_dupes <- data.frame(a = 1, stringsAsFactors = FALSE)
+
+test_that("instance of no dupes throws correct message, returns empty df", {
+  expect_warning(no_dupes %>% get_dupes(a), "No duplicate combinations found of: a")
+  expect_equal(suppressWarnings(no_dupes %>% get_dupes(a)), data_frame(a = double(0), dupe_count = integer(0)))
+  })
