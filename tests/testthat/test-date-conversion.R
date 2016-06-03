656fab0b774f14ef7e8040edf1a840f1277aae37
Sam Firke <samuel.firke@gmail.com>
2016-06-03 13:15:51 -0400

    add tests for bad 2nd argument (date system)

--- tests/testthat/test-date-conversion.R
+++ tests/testthat/test-date-conversion.R
@@ -11,4 +11,6 @@ test_that("Serial number dates convert correctly", {
 
 test_that("Bad inputs handled appropriately", {
   expect_error(excel_numeric_to_date("hello"), "argument `date_num` must be of class numeric")
+  expect_error(excel_numeric_to_date(40908, "bad string"), "argument 'created' must be one of 'mac pre-2011' or 'modern'")
+  expect_error(excel_numeric_to_date(40908, 4), "argument 'created' must be one of 'mac pre-2011' or 'modern'")
 })
