ba6c1471f3b83e90afdf0a68101566413e8c99bf
Sam Firke <samuel.firke@gmail.com>
2016-06-17 12:57:17 -0400

    updates to tests for tabyl, w/ df-as-input removed

--- tests/testthat/test-tabyl.R
+++ tests/testthat/test-tabyl.R
@@ -5,32 +5,25 @@ library(dplyr)
 context("tabyl")
 
 cyl_tbl <- tabyl(mtcars$cyl)
-cyl_tbl_pipe <- mtcars %>% tabyl(cyl)
-
-
-test_that("supplying a vector yields same results as passing a data frame w/ unquoted variable name", {
-  expect_identical(cyl_tbl %>% setNames(c("cyl","n","percent")),
-               cyl_tbl_pipe)
-})
 
 test_that("counts are accurate", {
-  expect_equal(cyl_tbl_pipe$cyl, c(4, 6, 8))
-  expect_equal(cyl_tbl_pipe$n, c(11, 7, 14))
+  expect_equal(cyl_tbl$mtcars_cyl, c(4, 6, 8))
+  expect_equal(cyl_tbl$n, c(11, 7, 14))
 })
 
 test_that("percentages are accurate", {
-  expect_equal(cyl_tbl_pipe$percent, c(11/32, 7/32, 14/32))
+  expect_equal(cyl_tbl$percent, c(11/32, 7/32, 14/32))
 })
 
 # Character input, with and without NA
 test_df <- data.frame(grp = c("a", "b", "b", "c"), stringsAsFactors = FALSE)
 test_df_na <- data.frame(grp = c("a", "b", "b", "c", NA), stringsAsFactors = FALSE)
-test_res <- test_df %>% tabyl(grp)
-test_res_na <- test_df_na %>% tabyl(grp)
+test_res <- tabyl(test_df$grp)
+test_res_na <- tabyl(test_df_na$grp)
 
 test_that("names are right", {
-  expect_equal(names(cyl_tbl_pipe), c("cyl", "n", "percent"))
-  expect_equal(names(test_res_na), c("grp", "n", "percent", "valid_percent"))
+  expect_equal(names(cyl_tbl), c("mtcars_cyl", "n", "percent"))
+  expect_equal(names(test_res_na), c("test_df_na_grp", "n", "percent", "valid_percent"))
 })
 
 test_that("NAs handled correctly", {
@@ -38,8 +31,10 @@ test_that("NAs handled correctly", {
   expect_equal(test_res_na$valid_percent, c(0.25, 0.5, 0.25, NA))
 })
 
-test_that("show_NA parameter works", {
-  expect_equal(test_res, test_df_na %>% tabyl(grp, show_na = FALSE))
+test_that("show_NA = FALSE parameter works", {
+  expect_equal(test_res %>%
+                 stats::setNames(c("test_df_na_grp", names(test_res)[-1])),
+               tabyl(test_df_na$grp, show_na = FALSE))
 })
 
 test_that("sorting is preserved for factors", {
@@ -47,9 +42,7 @@ test_that("sorting is preserved for factors", {
 })
 
 # missing factor levels shown, with and without NA
-fac_df <- data.frame(x = factor(c("a"), levels = c("b", "a")))
 fac <- factor(c("a"), levels = c("b", "a"))
-fac_df_na <- data.frame(x = factor(c("a", NA), levels = c("b", "a")))
 fac_na <- factor(c("a", NA), levels = c("b", "a"))
 
 
@@ -57,9 +50,6 @@ test_that("missing factor levels are displayed without NA values", {
   expect_equal(tabyl(fac)[[1]], factor(c("b","a"), levels = c("b", "a")))
   expect_equal(tabyl(fac)[[2]], c(NA, 1))
   expect_equal(tabyl(fac)[[3]], c(NA, 1))
-  expect_equal((fac_df %>% tabyl(x))[[1]], factor(c("b","a"), levels = c("b", "a")))
-  expect_equal((fac_df %>% tabyl(x))[[2]], c(NA, 1))
-  expect_equal((fac_df %>% tabyl(x))[[3]], c(NA, 1))
 })
 
 test_that("missing factor levels are displayed with NA values", {
@@ -67,18 +57,15 @@ test_that("missing factor levels are displayed with NA values", {
   expect_equal(tabyl(fac_na)[[2]], c(NA, 1, 1))
   expect_equal(tabyl(fac_na)[[3]], c(NA, 0.5, 0.5))
   expect_equal(tabyl(fac_na)[[4]], c(NA, 1, NA))
-  expect_equal((fac_df_na %>% tabyl(x))[[1]], factor(c("b","a", NA), levels = c("b", "a")))
-  expect_equal((fac_df_na %>% tabyl(x))[[2]], c(NA, 1, 1))
-  expect_equal((fac_df_na %>% tabyl(x))[[3]], c(NA, 0.5, 0.5))
-  expect_equal((fac_df_na %>% tabyl(x))[[4]], c(NA, 1, NA))
 })
   
 # check sort parameter
-sorted_test_df_na <- test_df_na %>% tabyl(grp, sort = TRUE)
-sorted_with_fac <- data.frame(grp = factor(c("a", "c", "c"), levels = letters[1:3])) %>%
-  tabyl(grp, sort = TRUE)
-sorted_with_na_and_fac <- data.frame(grp = factor(c("a", "c", "c", NA), levels = letters[1:3])) %>%
-  tabyl(grp, sort = TRUE)
+sorted_test_df_na <- tabyl(test_df_na$grp, sort = TRUE)
+sorted_with_fac <- data.frame(grp = factor(c("a", "c", "c"), levels = letters[1:3]))
+sorted_with_fac <- tabyl(sorted_with_fac$grp, sort = TRUE)
+
+sorted_with_na_and_fac <- data.frame(grp = factor(c("a", "c", "c", NA), levels = letters[1:3]))
+sorted_with_na_and_fac <- tabyl(sorted_with_na_and_fac$grp, sort = TRUE)
 
 test_that("sort parameter works", {
   expect_equal(sorted_test_df_na[[1]], c("b", "a", "c", NA))
@@ -91,8 +78,10 @@ test_that("sort parameter works", {
 
 # bad inputs
 
-test_that("failure occurs if 0 or multiple variables passed as dots when calling on a data.frame", {
-  expect_error(mtcars %>% tabyl(cyl, gear), "more than one variable name specified")
-  expect_error(mtcars %>% tabyl(), "no variable name specified")
-  expect_error(tabyl(list(1, 2)), "input must be a logical, numeric, or character vector, or a data.frame with a column name specified in '...'")
+test_that("failure occurs when passed a list", {
+  expect_error(tabyl(list(1, 2)), "input must be a logical, numeric, or character vector")
+})
+
+test_that("a piped name of dot turns into x", {
+  expect_equal(mtcars %>% .$gear %>% tabyl %>% names(.) %>% .[1], "x")
 })
