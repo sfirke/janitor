8f422842aff6ea81089dbff1c5dcd90813b6e1f3
Sam Firke <samuel.firke@gmail.com>
2016-06-19 15:35:54 -0400

    big update to NA cleaning function

--- tests/testthat/test-clean-NAs.R
+++ tests/testthat/test-clean-NAs.R
@@ -3,39 +3,38 @@
 library(janitor)
 context("recoding of string NA values into NAs")
 
-test_df <- data.frame(v1 = c(1, NA),
-                      v2 = c("NA", "#NAME?"),
-                      v3 = c("#N/A", "test")
+test_df <- data.frame(v1 = c(1, NA, 3),
+                      v_fac = c("a", "a", "b")
 )
-test_df$v4 <- c("N/A", "n/a") # this one will be character, not factor
-
-clean_df <- clean_NA_variants(test_df)
-clean_custom <- clean_NA_variants(test_df, "1")
-clean_custom_all <- clean_NA_variants(test_df, c(1, "test"))
+test_df$v2 = c("NA", "#NAME?", "n/a") # this one will be character, not factor
 
 test_that("values are converted to NAs in a single vector", {
-  expect_equal(clean_NA_vec(c(1,2), 1), c(NA, 2))
-  expect_equal(clean_NA_vec(c(1,2), c(1,2)), as.numeric(c(NA, NA)))
-  expect_equal(clean_NA_vec(c(1:4), 3:4), c(1:2, NA, NA))
-  expect_equal(clean_NA_vec(c("hi", "there"), c("oh", "hi")), c(NA, "there"))
-  expect_equal(clean_NA_vec(c("#N/A"), "#N/A"), as.character(NA))
+  expect_equal(convert_to_NA(c(1,2), 1), c(NA, 2))
+  expect_equal(convert_to_NA(c(1,2), c(1,2)), as.numeric(c(NA, NA)))
+  expect_equal(convert_to_NA(c(1:4), 3:4), c(1:2, NA, NA))
+  expect_equal(convert_to_NA(c("hi", "there"), c("oh", "hi")), c(NA, "there")) # values that aren't present are okay
+  expect_equal(convert_to_NA(c("#N/A"), "#N/A"), as.character(NA)) # does nothing with NA input
 })
 
-test_that("default values are cleaned at the data.frame level", {
-  expect_equal(clean_df[[1]], c(1, NA))  
-  expect_equivalent(clean_df[[2]], as.factor(c(NA, NA)))
-  expect_equivalent(clean_df[[3]], as.factor(c(NA, "test")))
-  expect_equal(clean_df[[4]], as.character(c(NA, NA)))
+cleaned_df <- convert_to_NA(test_df, c("NA", "#NAME?", "N/A", "n/a", 1, "a"))
+
+test_that("values are converted to NAs at the data.frame level", {
+  expect_equal(cleaned_df[[1]], c(NA, NA, 3))  
+  expect_equal(cleaned_df[[2]], factor(c(NA, NA, "b"), levels = c("a", "b")))
+  expect_equivalent(cleaned_df[[3]], as.character(rep(NA, 3)))
 })
 
-test_that("custom cleaning values are cleaned with clean_NA_variants", {
-  expect_true(is.na(clean_custom[1,1]))  
-  expect_false(is.na(clean_custom[2,3]))
-  expect_equal(sum(is.na(clean_custom_all)), 8) # all values should convert to NA
+# test bad inputs
+test_that("1st argument rejects bad inputs", {
+  expect_error(convert_to_NA(list("a"), 1), "argument 'dat' must be a vector or data.frame")
 })
 
-test_that("2nd argument rejects non-string vectors", {
-  expect_error(clean_NA_variants(test_df, 1), "addl_strings parameter should be a character vector, if specified")
-  expect_error(clean_NA_variants(test_df, as.factor("hi")), "addl_strings parameter should be a character vector, if specified")
-  expect_error(clean_NA_variants(test_df, list("1", "hi")), "addl_strings parameter should be a character vector, if specified")
+test_that("2nd argument rejects bad inputs", {
+  expect_error(convert_to_NA(test_df, list("1", "hi")), "'strings' parameter should be a vector of class character, numeric, factor, or integer")
+})
+
+# non-replacement warning
+test_that("non-replacement warning prints", {
+  expect_warning(convert_to_NA(mtcars, "gfjklj"), "no replacements made")
+  expect_warning(convert_to_NA(1, 2), "no replacements made")
 })
