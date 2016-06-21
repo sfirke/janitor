15484181b78a06e9e37c255ca70db83d926b1362
Sam Firke <samuel.firke@gmail.com>
2016-06-21 13:37:57 -0400

    addressing the NOTE caused by `.`

---
+++ R/janitor.R
@@ -0,0 +1,12 @@
+#' janitor
+#' 
+#' Examining and cleaning data
+#' 
+#' Package summary
+#' 
+#' @docType package
+#' @name janitor
+#' @importFrom dplyr %>%
+NULL
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
