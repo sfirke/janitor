aeeafeef788b6eb15f521c7baecab57484d32bb8
Sam Firke <samuel.firke@gmail.com>
2016-06-21 13:44:35 -0400

    adding n to globalVariables
    ala https://github.com/smbache/magrittr/issues/29

--- R/janitor.R
+++ R/janitor.R
@@ -9,4 +9,5 @@
 #' @importFrom dplyr %>%
 NULL
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+## and the "n" that is produced by dplyr::count() in a pipeline
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "n"))
