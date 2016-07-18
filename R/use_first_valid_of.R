a4087d4ec1abe06d64640c4b6497bdfd188b973a
Sam Firke <samuel.firke@gmail.com>
2016-07-18 07:11:02 -0400

    stub for use_first_valid_of

---
+++ R/use_first_valid_of.R
@@ -0,0 +1,8 @@
+use_first_valid_of <- function(...){
+  # check var lengths
+  # check var types?  Initially should be same class, maybe eventually coerce to character 
+  vars <- as.list(substitute(list(...)))
+  # initialize results vector of appropriate length and class
+  # fill it using 2 for loops
+  
+}
