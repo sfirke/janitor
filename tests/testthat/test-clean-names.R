a17938793f19eba7fb13be00455f80a8988450b2
Sam Firke <samuel.firke@gmail.com>
2016-08-18 22:12:57 -0400

    slight tweak to tests
    "repeat" is a reserved word, so switching

--- tests/testthat/test-clean-names.R
+++ tests/testthat/test-clean-names.R
@@ -4,20 +4,20 @@ library(janitor)
 context("data.frame name cleaning")
 
 test_df <- data.frame(matrix(ncol = 10) %>% as.data.frame())
-names(test_df) <- c("sp ace", "repeat", "a**#@", "%", "#", "!",
-                    "d(!)9", "REPEAT", "can\"'t", "hi_`there`")
+names(test_df) <- c("sp ace", "repeated", "a**#@", "%", "#", "!",
+                    "d(!)9", "REPEATED", "can\"'t", "hi_`there`")
 
 clean <- clean_names(test_df)
 
 test_that("Names are cleaned appropriately", {
   expect_equal(names(clean)[1], "sp_ace") # spaces
-  expect_equal(names(clean)[2], "repeat") # first instance of repeat
+  expect_equal(names(clean)[2], "repeated") # first instance of repeat
   expect_equal(names(clean)[3], "a") # multiple special chars, trailing special chars
   expect_equal(names(clean)[4], "percent") # converting % to percent
   expect_equal(names(clean)[5], "x") # 100% invalid name
   expect_equal(names(clean)[6], "x_2") # repeat of invalid name
   expect_equal(names(clean)[7], "d_9") # multiple special characters
-  expect_equal(names(clean)[8], "repeat_2") # uppercase, 2nd instance of repeat
+  expect_equal(names(clean)[8], "repeated_2") # uppercase, 2nd instance of repeat
   expect_equal(names(clean)[9], "cant") # uppercase, 2nd instance of repeat
   expect_equal(names(clean)[10], "hi_there") # double-underscores to single
 })
