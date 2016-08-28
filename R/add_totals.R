54770e14f26cafa1954f1830663f577c0b20694a
Sam Firke <samuel.firke@gmail.com>
2016-08-27 21:30:55 -0400

    added add_totals_row and _col functions

--- R/add_totals.R
+++ R/add_totals.R
@@ -1,27 +1,41 @@
-#' @title Append a totals row or column to a data.frame.
+#' @title Append a totals row to a data.frame.
 #'
 #' @description
-#' Both functions exclude the first column of the input data.frame, assuming that it contains a descriptive variable not to be summed.
+#' This function excludes the first column of the input data.frame, assuming that it contains a descriptive variable not to be summed.
 #'
-#' @param dat an input data.frame with numeric 
-#' @return Returns a data.frame.
+#' @param dat an input data.frame with numeric values in all columns beyond the first.
+#' @param na.rm should missing values (including NaN) be omitted from the calculations?
+#' @return Returns a data.frame with a totals row, consisting of "Total" in the first column and column sums in the others.
 #' @examples
 #' library(dplyr) # for the %>% pipe
 #' mtcars %>%
 #'   crosstab(am, cyl) %>%
-#'   add_totals_row %>%
-#'   add_totals_col
+#'   add_totals_row
 
 
-add_totals_row <- function(dat){
+add_totals_row <- function(dat, na.rm = TRUE){
   dat[[1]] <- as.character(dat[[1]]) # for binding to the "Total" character value of add-on row
-  col_totals <- data.frame(x1 = "Total", t(colSums(dat[,-1])), stringsAsFactors = FALSE) %>%
+  col_totals <- data.frame(x1 = "Total", t(colSums(dat[,-1], na.rm = na.rm)), stringsAsFactors = FALSE) %>%
     stats::setNames(names(dat))
   dplyr::bind_rows(dat, col_totals)
 }
 
-add_totals_col <- function(dat){
-  row_totals <- data.frame(Total = rowSums(dat[,-1]))
+#' @title Append a totals column to a data.frame.
+#'
+#' @description
+#' This function excludes the first column of the input data.frame, assuming that it contains a descriptive variable not to be summed.
+#'
+#' @param dat an input data.frame with numeric values in all columns beyond the first.
+#' @param na.rm should missing values (including NaN) be omitted from the calculations?
+#' @return Returns a data.frame with a totals column, consisting of "Total" in the first row and row sums in the others.
+#' @examples
+#' library(dplyr) # for the %>% pipe
+#' mtcars %>%
+#'   crosstab(am, cyl) %>%
+#'   add_totals_col
+
+add_totals_col <- function(dat, na.rm = TRUE){
+  row_totals <- data.frame(Total = rowSums(dat[,-1], na.rm = na.rm))
   dplyr::bind_cols(dat, row_totals)
 }
 
