094171de646e214cce64e6c3fac0d70c0b6bb1e6
Sam Firke <samuel.firke@gmail.com>
2016-10-30 13:56:47 -0400

    framework for coalesce_df tests

--- tests/testthat/test-coalesce-df.R
+++ tests/testthat/test-coalesce-df.R
@@ -4,9 +4,10 @@ library(janitor)
 library(dplyr)
 context("coalesece_dfs()")
 
-df1 <- data.frame(id = 1, lo = as.numeric(NA), med = 3, hi = as.numeric(NA), other = "still here")
-df2 <- data.frame(id = 1, med = 4, hi = 10, lo = 1)
+df1 <- data.frame(id = 1, lo = as.numeric(NA), med = 3, hi = as.numeric(NA), other = "still here", stringsAsFactors = FALSE)
+df2 <- data.frame(id = 1, med = 4, hi = 10, lo = 1, other = as.character(NA), stringsAsFactors = FALSE)
 coalesce_dfs(df1, df2, by = "id")
+coalesce_dfs(df1, df2, precedence = "y", by = "id")
 #' coalesce_dfs(t1, t2, vars_to_update = c("lo"), by = "id") # update just the specified col
 
 # need to add error catching in the function, and then test that erroring behavior here
