2279ed48db2b1e9557a3c8528155b3dee9b749d3
Ryan Knight <ryangknight@gmail.com>
2016-11-09 16:05:20 -0500

    check_dupes implementation, start of get_dupes rewrite

---
+++ R/check_dupes.R
@@ -0,0 +1,59 @@
+#' @title Check if there are duplicates in the supplied columns
+#'
+#' @description
+#' Check whether the supplied columns have duplicates and, 
+#' if there are duplicates, handle at the supplied level.
+#'
+#' @param df the input data frame
+#' @param ...  unquoted variable names to search for duplicates
+#' @param level What should happen if there are duplicates. Can be "stop", "warn", "message" or "logical". Logical returns TRUE or FALSE. 
+#' @return If level == "logical", TRUE/FALSE, otherwise the data frame is returned silently for further piping
+#' @export
+#' @examples
+#' # Make sure no duplicate cars were added since the last time we loaded the data
+#' mtcars %>%
+#'  mutate(make = row.names(.)) %>%
+#'  dups_check(make)
+#'
+#' # Check whether any Species have the same average Sepal.Width
+#' iris %>%
+#'  group_by(Species) %>%
+#'  summarise(ave_sepal_width = mean(Sepal.Width)) %>%
+#'  dups_check(ave_sepal_width, level="warn") %>%
+#'  select(Species) # Silly example of pipe-ability
+
+check_dupes <- function(df, ..., level="stop"){
+  
+  # Note: Would be nice to be able to set the default level once...
+  levels <- c("stop", "warn", "message", "logical")
+  
+  if (!(level %in% levels)){
+    stop(sprintf("level must be one of %s", paste0(levels, collapse = ", ")))
+  }
+  
+  vars <- lazyeval::lazy_dots(...)
+  target <- df %>% select_(.dots=vars)
+  
+  num_dups <- sum(duplicated(target))
+  
+  dup_message <- sprintf("There are %d duplicates", num_dups) # Would be nice to parse the df name instead of gettting "." when piped
+  
+  if (level == "logical"){
+    return(num_dups != 0) # Logical returns true/false only
+  }
+  else if (num_dups == 0){
+    return(invisible(df))
+  }
+  else if (level == "message"){
+    message(dup_message)
+  }
+  else if (level == "warn"){
+    warning(dup_message)
+  }
+  else if (level == "stop"){
+    stop(dup_message)
+  }
+  invisible(df)
+}
+
+
--- R/get_dupes.R
+++ R/get_dupes.R
@@ -1,63 +1,8 @@
-#' @title Get rows of a \code{data.frame} with identical values for the specified variables.
-#'
-#' @description
-#' For hunting duplicate records during data cleaning.  Specify the data.frame and the variable combination to search for duplicates and get back the duplicated rows.
-#'
-#' @param dat the input data.frame.
-#' @param ... unquoted variable names to search for duplicates.
-#' @return Returns a data.frame (actually a \code{tbl_df}) with the full records where the specified variables have duplicated values, as well as a variable \code{dupe_count} showing the number of rows sharing that combination of duplicated values.
 #' @export
-#' @examples
-#' get_dupes(mtcars, mpg, hp)
-#' # or called with magrittr pipe %>% :
-#' library(dplyr)
-#' mtcars %>% get_dupes(wt)
-#'
-
-get_dupes <- function(dat, ...) {
-  names <- as.list(substitute(list(...)))[-1L]
-  df_name <- deparse(substitute(dat))
+get_dupes <- function(df, ...){
+  vars <- lazyeval::lazy_dots(...)
+  target <- df %>% select_(.dots = vars)
+  dup_index <- duplicated(target) | duplicated(target, fromLast = TRUE)
   
-  # check that each variable name provided is present in names(dat); if not, throw error
-  var_names <- names
-  if(is.list(var_names)){ var_names <- lapply(names, deparse) } # 'names' is not a list if defaulting to whole df, need this for consistency
-  check_vars_in_df(dat, df_name, unlist(var_names))
-  dupe_count <- NULL # to appease NOTE for CRAN; does nothing.
-  
-  if(length(names)==0){ # if called on an entire data.frame with no specified variable names
-    var_names <- names(dat)
-    names <- paste0("`", as.list(names(dat)), "`") # to handle illegal variable names 
-    message("No variable names specified - using all columns.\n")
-  }
-  
-  # calculate counts to join back to main df
-  counts <- dat %>%
-    dplyr::count_(vars = names)
-  
-  
-  # join new count vector to main data.frame
-  dupes <- suppressMessages(dplyr::inner_join(counts, dat))
-  
-  dupes <- dupes %>%
-    dplyr::filter(n > 1) %>%
-    dplyr::ungroup() %>%
-    dplyr::arrange_(.dots = names) %>%
-    dplyr::rename(dupe_count = n)
-  
-  # shorten error message for large data.frames
-  if(length(var_names) > 10){ var_names <- c(var_names[1:9], paste("... and", length(var_names) - 9, "other variables")) }
-  if(nrow(dupes) == 0){message(paste0("No duplicate combinations found of: ", paste(var_names, collapse = ", ")))}
-  dupes
-}
-
-# takes a data.frame and vector of variable names, confirms that all var names match data.frame names 
-check_vars_in_df <- function(dat, dat_name, names_vec){
-  in_df <- unlist(lapply(names_vec, function(x) x %in% names(dat)))
-  if(sum(in_df) != length(in_df)){
-    stop(paste0(
-      paste0("These variables do not match column names in ", dat_name, ": "),
-      paste(names_vec[!in_df], collapse =", ")
-    )
-    )
-  }
+  df[dup_index, ]
 }
