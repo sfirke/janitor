28fafcb99142aac029602658057491ca2d5668e5
Sam Firke <samuel.firke@gmail.com>
2017-04-12 12:37:47 -0400

    export the %>% pipe from magrittr
    closes #107

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+# Copied from tidyr/R/utils.R, to export the magrittr pipe
+
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
