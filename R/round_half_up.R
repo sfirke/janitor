876ff0e26a82a3bb09fb77fa7fa3ec196b89fb7e
Sam Firke <samuel.firke@gmail.com>
2017-08-25 10:34:20 -0400

    have round_half_up default to digits = 0 to match base R round()

--- R/round_half_up.R
+++ R/round_half_up.R
@@ -9,12 +9,12 @@
 #' @param digits how many digits should be displayed after the decimal point?
 #' @export
 #' @examples
-#' round_half_up(12.5, 0)
+#' round_half_up(12.5)
 #' round_half_up(1.125, 2)
 #' round_half_up(1.125, 1)
 #' round_half_up(-0.5, 0) # negatives get rounded away from zero
 #' 
-round_half_up <- function(x, digits){
+round_half_up <- function(x, digits = 0){
   posneg = sign(x)
   z = abs(x) * 10 ^ digits
   z = z + 0.5
