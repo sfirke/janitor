79237712a7d796f3a5c9851db93bf57b8bba13b0
Sam Firke <samuel.firke@gmail.com>
2017-09-29 14:32:51 -0400

    restore deprecated adorn_crosstab helpers
    so adorn_crosstab can work until/if it's removed

--- R/adorn_crosstab.R
+++ R/adorn_crosstab.R
@@ -46,4 +46,40 @@ adorn_crosstab <- function(dat, denom = "row", show_n = TRUE, digits = 1, show_t
   
 }
 
+## These are legacy helper sub-functions for adorn_crosstab().  They've been replaced with better helpers for adorn_ns().  Keep here until adorn_crosstab() is removed, then remove these also.
+
+# takes data.frames of Ns and %s, pastes them together
+paste_ns <- function(perc_df, n_df){
+  n_matrix <- as.matrix(n_df)
+  perc_matrix <- as.matrix(perc_df)
+  
+  # paste the results together
+  pasted <- paste(perc_matrix, " (", n_matrix, ")", sep = "") %>% # paste the matrices
+    sapply(., fix_parens_whitespace) %>% # apply the whitespace cleaning function to the resulting vector
+    matrix(., nrow = nrow(n_matrix), dimnames = dimnames(perc_matrix)) %>% # cast as matrix, then data.frame
+    dplyr::as_data_frame(pasted)
+  
+  pasted[[1]] <- n_df[[1]] # undo the pasting in this 1st column
+  pasted
+}
+
+# converts "50.0% ( 1)" to "50.0%  (1)" for nice printing 
+fix_parens_whitespace <- function(x){
+  culprit <- regmatches(x, regexpr("[(][ ]+", x)) # isolate the problematic string
+  
+  # if no problem, return unmodified
+  if(length(culprit) == 0){ x }
+  
+  else{
+    num_spaces <- length(gregexpr(" ", culprit)[[1]])
+    gsub(culprit[[1]],
+         paste0( # create replacement string
+           paste0(rep(" ", num_spaces), collapse = ""), # generate the spaces
+           "(",
+           collapse = ""),
+         x,
+         fixed = TRUE)
+  }
+  
+}
 
