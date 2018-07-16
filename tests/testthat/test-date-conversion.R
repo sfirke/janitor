8795a7f3ccb32429630d04a6cdb07950a55ca1da
Bill Denney <billdenney@users.noreply.github.com>
2018-07-16 15:10:54 -0400

    Correct second handling so that 60 seconds are not possible (#218)
    * Correct second handling so that 60 seconds are not possible
    
    * Manage date rollover within 0.001 sec

--- tests/testthat/test-date-conversion.R
+++ tests/testthat/test-date-conversion.R
@@ -29,3 +29,22 @@ test_that("time handling works correctly", {
                as.Date("2016-01-01"),
                info="Time inclusion works with a fractional date/time and only the date kept.")
 })
+
+test_that("time handling at the edge of a minute works correctly", {
+  expect_equal(excel_numeric_to_date(42001.1, include_time = TRUE),
+               as.POSIXlt("2014-12-28 02:24:00"),
+               info = "60 seconds gets converted to 0 seconds, +1 minute")
+})
+
+test_that("time handling at the edge of the next date works correctly", {
+  expect_warning(excel_numeric_to_date(42002 - 0.0005/86400, include_time=TRUE, round_seconds=FALSE),
+                 regexp="1 date_num values are within 0.001 sec of a later date and were rounded up to the next day.")
+  expect_equal(excel_numeric_to_date(42002 - 0.0005/86400, include_time=TRUE, round_seconds=FALSE),
+               as.POSIXlt("2014-12-29"))
+  expect_equal(excel_numeric_to_date(42002 - 0.0005/86400, include_time=TRUE, round_seconds=TRUE),
+               as.POSIXlt("2014-12-29"))
+  expect_equal(excel_numeric_to_date(42002 - 0.0011/86400, include_time=TRUE, round_seconds=FALSE),
+               as.POSIXlt("2014-12-28 23:59:59.998"))
+  expect_equal(excel_numeric_to_date(42002 - 0.0011/86400, include_time=TRUE, round_seconds=TRUE),
+               as.POSIXlt("2014-12-29"))
+})
