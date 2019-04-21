be3b29b1c55e171b334a38ad2fb73975f3c5fb30
Sam Firke <samuel.firke@gmail.com>
2019-04-20 23:57:49 -0400

    update pkgdown site

--- docs/pkgdown.js
+++ docs/pkgdown.js
@@ -1,115 +1,99 @@
-/* http://gregfranko.com/blog/jquery-best-practices/ */
-(function($) {
-  $(function() {
+$(function() {
 
-    $("#sidebar")
-      .stick_in_parent({offset_top: 40})
-      .on('sticky_kit:bottom', function(e) {
-        $(this).parent().css('position', 'static');
-      })
-      .on('sticky_kit:unbottom', function(e) {
-        $(this).parent().css('position', 'relative');
-      });
-
-    $('body').scrollspy({
-      target: '#sidebar',
-      offset: 60
+  $("#sidebar")
+    .stick_in_parent({offset_top: 40})
+    .on('sticky_kit:bottom', function(e) {
+      $(this).parent().css('position', 'static');
+    })
+    .on('sticky_kit:unbottom', function(e) {
+      $(this).parent().css('position', 'relative');
     });
 
-    $('[data-toggle="tooltip"]').tooltip();
+  $('body').scrollspy({
+    target: '#sidebar',
+    offset: 60
+  });
 
-    var cur_path = paths(location.pathname);
-    var links = $("#navbar ul li a");
-    var max_length = -1;
-    var pos = -1;
-    for (var i = 0; i < links.length; i++) {
-      if (links[i].getAttribute("href") === "#")
-        continue;
-      // Ignore external links
-      if (links[i].host !== location.host)
-        continue;
+  var cur_path = paths(location.pathname);
+  $("#navbar ul li a").each(function(index, value) {
+    if (value.text == "Home")
+      return;
+    if (value.getAttribute("href") === "#")
+      return;
 
-      var nav_path = paths(links[i].pathname);
-
-      var length = prefix_length(nav_path, cur_path);
-      if (length > max_length) {
-        max_length = length;
-        pos = i;
-      }
-    }
-
-    // Add class to parent <li>, and enclosing <li> if in dropdown
-    if (pos >= 0) {
-      var menu_anchor = $(links[pos]);
+    var path = paths(value.pathname);
+    if (is_prefix(cur_path, path)) {
+      // Add class to parent <li>, and enclosing <li> if in dropdown
+      var menu_anchor = $(value);
       menu_anchor.parent().addClass("active");
       menu_anchor.closest("li.dropdown").addClass("active");
     }
   });
+});
 
-  function paths(pathname) {
-    var pieces = pathname.split("/");
-    pieces.shift(); // always starts with /
+function paths(pathname) {
+  var pieces = pathname.split("/");
+  pieces.shift(); // always starts with /
 
-    var end = pieces[pieces.length - 1];
-    if (end === "index.html" || end === "")
-      pieces.pop();
-    return(pieces);
+  var end = pieces[pieces.length - 1];
+  if (end === "index.html" || end === "")
+    pieces.pop();
+  return(pieces);
+}
+
+function is_prefix(needle, haystack) {
+  if (needle.length > haystack.lengh)
+    return(false);
+
+  // Special case for length-0 haystack, since for loop won't run
+  if (haystack.length === 0) {
+    return(needle.length === 0);
   }
 
-  // Returns -1 if not found
-  function prefix_length(needle, haystack) {
-    if (needle.length > haystack.length)
-      return(-1);
-
-    // Special case for length-0 haystack, since for loop won't run
-    if (haystack.length === 0) {
-      return(needle.length === 0 ? 0 : -1);
-    }
-
-    for (var i = 0; i < haystack.length; i++) {
-      if (needle[i] != haystack[i])
-        return(i);
-    }
-
-    return(haystack.length);
+  for (var i = 0; i < haystack.length; i++) {
+    if (needle[i] != haystack[i])
+      return(false);
   }
 
-  /* Clipboard --------------------------*/
+  return(true);
+}
 
-  function changeTooltipMessage(element, msg) {
-    var tooltipOriginalTitle=element.getAttribute('data-original-title');
-    element.setAttribute('data-original-title', msg);
-    $(element).tooltip('show');
-    element.setAttribute('data-original-title', tooltipOriginalTitle);
-  }
+/* Clipboard --------------------------*/
 
-  if(ClipboardJS.isSupported()) {
-    $(document).ready(function() {
-      var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' type = 'submit' title='Copy to clipboard' aria-label='Copy to clipboard' data-toggle='tooltip' data-placement='left auto' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy'></i></button>";
+function changeTooltipMessage(element, msg) {
+  var tooltipOriginalTitle=element.getAttribute('data-original-title');
+  element.setAttribute('data-original-title', msg);
+  $(element).tooltip('show');
+  element.setAttribute('data-original-title', tooltipOriginalTitle);
+}
 
-      $(".examples, div.sourceCode").addClass("hasCopyButton");
+if(Clipboard.isSupported()) {
+  $(document).ready(function() {
+    var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' type = 'submit' title='Copy to clipboard' aria-hidden='true' data-toggle='tooltip' data-placement='left auto' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy' aria-hidden='true'></i></button>";
 
-      // Insert copy buttons:
-      $(copyButton).prependTo(".hasCopyButton");
+    $(".examples").addClass("hasCopyButton");
 
-      // Initialize tooltips:
-      $('.btn-copy-ex').tooltip({container: 'body'});
+    // Insert copy buttons:
+    $(copyButton).prependTo(".hasCopyButton");
 
-      // Initialize clipboard:
-      var clipboardBtnCopies = new ClipboardJS('[data-clipboard-copy]', {
-        text: function(trigger) {
-          return trigger.parentNode.textContent;
-        }
-      });
+    // Initialize tooltips:
+    $('.btn-copy-ex').tooltip({container: 'body'});
 
-      clipboardBtnCopies.on('success', function(e) {
-        changeTooltipMessage(e.trigger, 'Copied!');
-        e.clearSelection();
-      });
-
-      clipboardBtnCopies.on('error', function() {
-        changeTooltipMessage(e.trigger,'Press Ctrl+C or Command+C to copy');
-      });
+    // Initialize clipboard:
+    var clipboardBtnCopies = new Clipboard('[data-clipboard-copy]', {
+      text: function(trigger) {
+        return trigger.parentNode.textContent;
+      }
     });
-  }
-})(window.jQuery || window.$)
+
+    clipboardBtnCopies.on('success', function(e) {
+      changeTooltipMessage(e.trigger, 'Copied!');
+      e.clearSelection();
+    });
+
+    clipboardBtnCopies.on('error', function() {
+      changeTooltipMessage(e.trigger,'Press Ctrl+C or Command+C to copy');
+    });
+  });
+}
+
