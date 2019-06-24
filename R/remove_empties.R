af42ea8850e29d970a5a47aeb01a0c52447f6056
Bill Denney <wdenney@humanpredictions.com>
2019-06-24 10:42:26 -0400

    reformatting of documentation

--- R/remove_empties.R
+++ R/remove_empties.R
@@ -1,18 +1,20 @@
 #' @title Remove empty rows and/or columns from a data.frame or matrix.
 #'
-#' @description
-#' Removes all rows and/or columns from a data.frame or matrix that are composed entirely of \code{NA} values.
+#' @description Removes all rows and/or columns from a data.frame or matrix that
+#' are composed entirely of \code{NA} values.
 #'
 #' @param dat the input data.frame or matrix.
-#' @param which one of "rows", "cols", or \code{c("rows", "cols")}.  Where no value of which is provided, defaults to removing both empty rows and empty columns, declaring the behavior with a printed message.
+#' @param which one of "rows", "cols", or \code{c("rows", "cols")}.  Where no
+#'   value of which is provided, defaults to removing both empty rows and empty
+#'   columns, declaring the behavior with a printed message.
 #' @return Returns the object without its missing rows or columns.
 #' @family remove functions
-#' @seealso \code{\link[=remove_constant]{remove_constant()}} for removing constant
-#' columns.
-#' @export
+#' @seealso \code{\link[=remove_constant]{remove_constant()}} for removing
+#'   constant columns.
 #' @examples
 #' # not run:
 #' # dat %>% remove_empty("rows")
+#' @export
 
 remove_empty <- function(dat, which = c("rows", "cols")) {
   if (missing(which) && !missing(dat)) {
@@ -36,21 +38,21 @@ remove_empty <- function(dat, which = c("rows", "cols")) {
 
 #' @title Remove constant columns from a data.frame or matrix.
 #' @param dat the input data.frame or matrix.
-#' @param na.rm should \code{NA} values be removed when considering whether a 
-#' column is constant?  The default value of \code{FALSE} will result in a column 
-#' not being removed if it's a mix of a single value and \code{NA}.
-#' 
+#' @param na.rm should \code{NA} values be removed when considering whether a
+#'   column is constant?  The default value of \code{FALSE} will result in a
+#'   column not being removed if it's a mix of a single value and \code{NA}.
+#'
 #' @examples
 #' remove_constant(data.frame(A=1, B=1:3))
-#' 
+#'
 #' # To find the columns that are constant
 #' data.frame(A=1, B=1:3) %>%
 #'   dplyr::select_at(setdiff(names(.), names(remove_constant(.)))) %>%
 #'   unique()
 #' @importFrom stats na.omit
 #' @family remove functions
-#' @seealso \code{\link[=remove_empty]{remove_empty()}} for removing empty 
-#' columns or rows.
+#' @seealso \code{\link[=remove_empty]{remove_empty()}} for removing empty
+#'   columns or rows.
 #' @export
 remove_constant <- function(dat, na.rm = FALSE) {
   mask <-
@@ -80,10 +82,10 @@ remove_constant <- function(dat, na.rm = FALSE) {
 #'
 #' @param dat the input data.frame.
 #' @return Returns the data.frame with no empty rows.
-#' @export
 #' @examples
 #' # not run:
 #' # dat %>% remove_empty_rows
+#' @export
 
 remove_empty_rows <- function(dat) {
   .Deprecated("remove_empty(\"rows\")")
@@ -97,11 +99,10 @@ remove_empty_rows <- function(dat) {
 #'
 #' @param dat the input data.frame.
 #' @return Returns the data.frame with no empty columns.
-#' @export
 #' @examples
 #' # not run:
 #' # dat %>% remove_empty_cols
-#'
+#' @export
 
 remove_empty_cols <- function(dat) {
   .Deprecated("remove_empty(\"cols\")")
