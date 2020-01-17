5baf06260c4f0290b5533910e5c3cbd71e503998
Jon Zadra <jon.zadra@sorensonimpact.com>
2020-01-17 16:20:52 -0700

    switch variable selection to using tidyselect.  Remove old check_vars_in_df() function for matching variable names as eval_select() now handles it.

--- R/get_dupes.R
+++ R/get_dupes.R
@@ -3,47 +3,48 @@
 #' @description
 #' For hunting duplicate records during data cleaning.  Specify the data.frame and the variable combination to search for duplicates and get back the duplicated rows.
 #'
-#' @param dat the input data.frame.
-#' @param ... unquoted variable names to search for duplicates.
+#' @param dat The input data.frame.
+#' @param ... Unquoted variable names to search for duplicates. This takes a tidyselect specification.
 #' @return Returns a data.frame (actually a \code{tbl_df}) with the full records where the specified variables have duplicated values, as well as a variable \code{dupe_count} showing the number of rows sharing that combination of duplicated values.
 #' @export
 #' @examples
 #' get_dupes(mtcars, mpg, hp)
 #' # or called with the magrittr pipe %>% :
 #' mtcars %>% get_dupes(wt)
+#' # or using tidyselect:
+#' mtcars %>% get_dupes(weight = wt, starts_with("cy"))
 #'
 
 get_dupes <- function(dat, ...) {
-  uq_names <- as.list(substitute(list(...)))[-1L] # unquoted names for NSE calls, need quoted names separately for messages + warnings
-  df_name <- deparse(substitute(dat))
-
-  if (length(uq_names) == 0) { # if called on an entire data.frame with no specified variable names
+  
+  expr <- rlang::expr(c(...))
+  pos <- tidyselect::eval_select(expr, data = dat)
+  
+  names(dat)[pos] <- names(pos) #allows for renaming within get_dupes() consistent with select()
+  
+  if (rlang::dots_n(...) == 0) { # if no tidyselect variables are specified, check the whole data.frame
     var_names <- names(dat)
     nms <- rlang::syms(var_names)
     message("No variable names specified - using all columns.\n")
   } else {
-    nms <- rlang::quos(...) %>%
-      rlang::quos_auto_name()
-    var_names <- names(nms)
+    var_names <- names(pos)
+    nms <- rlang::syms(var_names)
   }
-
-  # check that each variable name provided is present in names(dat); if not, throw error
-  check_vars_in_df(dat, df_name, var_names)
+  
   dupe_count <- NULL # to appease NOTE for CRAN; does nothing.
-
+  
   # calculate counts to join back to main df
   counts <- dat %>%
-    dplyr::count(!!! nms)
-
-  names(counts)[ncol(counts)] <- "dupe_count"
+    dplyr::count(!!! nms, name = "dupe_count")
+  
   # join new count vector to main data.frame
   dupes <- suppressMessages(dplyr::inner_join(counts, dat))
-
+  
   dupes <- dupes %>%
     dplyr::filter(dupe_count > 1) %>%
     dplyr::ungroup() %>%
     dplyr::arrange(!!! nms)
-
+  
   # shorten error message for large data.frames
   if (length(var_names) > 10) {
     var_names <- c(var_names[1:9], paste("... and", length(var_names) - 9, "other variables"))
@@ -54,13 +55,4 @@ get_dupes <- function(dat, ...) {
   dupes
 }
 
-# takes a data.frame and vector of variable names, confirms that all var names match data.frame names
-check_vars_in_df <- function(dat, dat_name, names_vec) {
-  in_df <- unlist(lapply(names_vec, function(x) x %in% names(dat)))
-  if (sum(in_df) != length(in_df)) {
-    stop(paste0(
-      paste0("These variables do not match column names in ", dat_name, ": "),
-      paste(names_vec[!in_df], collapse = ", ")
-    ))
-  }
-}
+
