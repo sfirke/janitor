c620db1e7a03b0143f68deb03e01a1d218354e72
JJSteph <John.J.Stephen@Gmail.com>
2020-08-17 20:17:01 -0500

    Address issue #396

--- R/round_half_up.R
+++ R/round_half_up.R
@@ -17,7 +17,7 @@
 round_half_up <- function(x, digits = 0) {
   posneg <- sign(x)
   z <- abs(x) * 10 ^ digits
-  z <- z + 0.5
+  z <- z + 0.5 + sqrt(.Machine$double.eps)
   z <- trunc(z)
   z <- z / 10 ^ digits
   z * posneg
