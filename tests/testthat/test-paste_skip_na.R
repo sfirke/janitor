0c89591b6343e750e9da5152545e35f7d99b5dba
Bill Denney <wdenney@humanpredictions.com>
2023-04-18 13:08:24 -0400

    Improve test coverage

--- tests/testthat/test-paste_skip_na.R
+++ tests/testthat/test-paste_skip_na.R
@@ -11,4 +11,15 @@ test_that("paste_skip_na", {
   expect_equal(paste_skip_na("A", NA, collapse = ","), "A")
   expect_equal(paste_skip_na("A", NA, c(NA, "B"), collapse = ","), "A,A B")
   expect_equal(paste_skip_na("A", NA, c(NA, "B"), sep = ","), c("A", "A,B"))
+
+  expect_equal(paste_skip_na(c("A", "B"), NA), c("A", "B"))
+  expect_equal(paste_skip_na(NA, c("A", "B")), c("A", "B"))
+})
+
+test_that("paste_skip_na expected errors", {
+  expect_error(
+    paste_skip_na(c("A", "B"), c("A", "B", "C")),
+    regexp = "Arguments must be the same length or one argument must be a scalar.",
+    fixed = TRUE
+  )
 })
